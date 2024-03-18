c3924d4de0701f189185e57dc728488b51ccbe11
Herdy <54350341+herdyhardiyant@users.noreply.github.com>
2024-03-19 03:53:01 +0700

    iterators

--- src/lib.rs
+++ src/lib.rs
@@ -24,50 +24,52 @@ pub struct Config {
 }
 
 impl Config {
-    pub fn build(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("not enough argument");
-        }
+    pub fn build(mut args: impl Iterator<Item = String>) -> Result<Config, &'static str> {
+        args.next();
 
-        let query = args[1].clone();
-        let file_path = args[2].clone();
+        let query = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a query string"),
+        };
+        let file_path = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get file path"),
+        };
         let ignore_case = env::var("IGNORE_CASE").is_ok();
 
-        Ok(Config { query, file_path, ignore_case })
+        Ok(Config {
+            query,
+            file_path,
+            ignore_case,
+        })
     }
 }
 
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    let mut results = Vec::new();
-
-    for line in contents.lines(){
-        if line.contains(query){
-            results.push(line.trim())
-        }
-    }
-
-    results
+    contents
+        .lines()
+        .filter(|line| line.contains(query))
+        .map(|word| word.trim())
+        .collect()
 }
 
-fn search_case_sensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str>{
+fn search_case_sensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
     let query = query.to_lowercase();
-    let mut results = Vec::new();
+    let query = query.as_str();
 
-    for line in contents.lines(){
-        if line.to_lowercase().contains(&query){
-            results.push(line.trim())
-        }
-    }
-
-    results
+    contents
+        .lines()
+        .filter(|line| line.to_lowercase().contains(query))
+        .map(|word| word.trim())
+        .collect()
 }
-
 
 #[cfg(test)]
 mod tests {
     use super::*;
+
     #[test]
-    fn one_result(){
+    fn one_result() {
         let query = "duct";
         let contents = "\
         Rust:
@@ -79,7 +81,7 @@ mod tests {
     }
 
     #[test]
-    fn case_sensitive(){
+    fn case_sensitive() {
         let query = "rUst";
         let contents = "\
         Rust:
@@ -87,9 +89,9 @@ mod tests {
         Pick three.
         Trust me.";
 
-        assert_eq!(vec!["Rust:", "Trust me." ], search_case_sensitive(&query, &contents))
-
-
+        assert_eq!(
+            vec!["Rust:", "Trust me."],
+            search_case_sensitive(&query, &contents)
+        )
     }
-
 }
